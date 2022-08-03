7779c672310962d75b138841c402ebc54aee7b42
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-03 16:22:03 +0900

    ci: add release workflow

---
+++ _tools/meta.ts
@@ -0,0 +1,40 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.29.1/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {},
+  typeCheck: true,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name: "isxx",
+    version,
+    description: "TypeScript-first is functions collection",
+    license: "MIT",
+    sideEffects: false,
+    author: {
+      name: "TomokiMiyauci",
+      email: "contact@miyauchi.dev",
+      url: "https://miyauchi.dev/",
+    },
+    repository: {
+      type: "git",
+      url: "https://github.com/TomokiMiyauci/isx.git",
+    },
+    bugs: {
+      url: "https://github.com/TomokiMiyauci/isx/issues",
+    },
+    homepage: "https://github.com/TomokiMiyauci/isx#readme",
+    funding: {
+      type: "patreon",
+      url: "https://www.patreon.com/tomoki_miyauci",
+    },
+    keywords: [
+      "is",
+      "function",
+      "utility",
+      "collection",
+    ],
+  },
+  packageManager: "pnpm",
+});
