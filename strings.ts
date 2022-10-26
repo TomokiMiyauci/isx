b291ae489e922fe89f555234ed42df53957858ba
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2022-10-26 12:27:34 +0900

    perf(strings): remove template literal for tree-shaking

--- strings.ts
+++ strings.ts
@@ -75,8 +75,6 @@ export function isRfc3339DateFormat(value: string): value is DateFormat {
 const FULL_TIME =
   "(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:Z|[+-][01]\\d:[0-5]\\d)";
 
-const DATE_TIME = /* @__PURE__ */ `${FULL_DATE}T${FULL_TIME}`;
-
 /** Whether the value is RFC 3339 time format or not.
  * The format compliant with {@link https://www.rfc-editor.org/rfc/rfc3339#section-5.6 RFC 3339, 5.6. Internet Date/Time Format, full-time}
  * @param value - Any `string`.
@@ -108,5 +106,5 @@ export function isRfc3339TimeFormat(value: string): value is TimeFormat {
 export function isRfc3339DateTimeFormat(
   value: string,
 ): value is DateTimeFormat {
-  return new RegExp(`^${DATE_TIME}$`).test(value);
+  return new RegExp(`^${`${FULL_DATE}T${FULL_TIME}`}$`).test(value);
 }
