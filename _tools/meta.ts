ddb39fc674ea0f62c495ff5d982eff8148808f4f
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-26 14:06:04 +0900

    fix(_tools): fix npm build script for generate types versions

--- _tools/meta.ts
+++ _tools/meta.ts
@@ -11,20 +11,35 @@ import {
   relative,
 } from "https://deno.land/std@0.181.0/path/mod.ts";
 
-function path2EntryPoint(path: string): EntryPoint {
-  const parsed = parse(path);
-  const name = join(parsed.dir, parsed.name.replaceAll("_", "-"));
+interface ModuleInfo {
+  readonly name: string;
+  readonly path: string;
+}
 
+function path2EntryPoint(module: ModuleInfo): EntryPoint {
   const entryPoint: EntryPoint = {
-    name: toRelative(name),
-    path: toRelative(path),
+    name: toRelative(module.name),
+    path: toRelative(module.path),
   };
 
   return entryPoint;
 }
 
+function module2TypeVersions(modules: readonly ModuleInfo[]) {
+  const entries = modules.map(({ name, path }) => {
+    return [name, [join("types", toDts(path))]];
+  });
+  const map = Object.fromEntries(entries);
+
+  return { "*": map };
+}
+
 function toRelative(path: string): string {
   return path.startsWith("./") ? path : "./" + path;
+}
+
+function toDts(path: string): string {
+  return path.replace(/.ts$/, ".d.ts");
 }
 
 export const makeOptions = (version: string): BuildOptions => {
@@ -34,9 +49,17 @@ export const makeOptions = (version: string): BuildOptions => {
     root,
   });
 
-  const entryPoints = [...entries]
-    .map(({ path }) => relative(root, path))
-    .map(path2EntryPoint);
+  const modules = [...entries].map(({ path }) => relative(root, path)).map(
+    (path) => {
+      const parsed = parse(path);
+      const name = join(parsed.dir, parsed.name);
+
+      return { name, path };
+    },
+  );
+
+  const entryPoints = modules.map(path2EntryPoint);
+  const typesVersions = module2TypeVersions(modules);
 
   return {
     test: false,
@@ -77,6 +100,8 @@ export const makeOptions = (version: string): BuildOptions => {
         "type-guard",
       ],
       publishConfig: { access: "public" },
+
+      typesVersions,
     },
     packageManager: "pnpm",
   };
