6f1b72af8325f8da2a2c3238c7c9a9e7385e47bb
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-27 11:19:46 +0900

    docs(iterable): add @param JSDOC

--- iterable/is_empty.ts
+++ iterable/is_empty.ts
@@ -2,6 +2,8 @@
 // This module is browser compatible.
 
 /** Weather the string is empty or not.
+ * @param input Any string.
+ *
  * @example
  * ```ts
  * import { isEmpty } from "https://deno.land/x/isx@$VERSION/iterable/is_empty.ts";
@@ -13,6 +15,8 @@
  */
 export function isEmpty(input: string): input is "";
 /** Weather the array is empty or not.
+ * @param input Any array.
+ *
  * @example
  * ```ts
  * import { isEmpty } from "https://deno.land/x/isx@$VERSION/iterable/is_empty.ts";
@@ -24,6 +28,8 @@ export function isEmpty(input: string): input is "";
  */
 export function isEmpty(input: readonly unknown[]): input is readonly [];
 /** Wether the input is empty or not.
+ * @param input Any iterable.
+ *
  * @example
  * ```ts
  * import { isEmpty } from "https://deno.land/x/isx@$VERSION/iterable/is_empty.ts";
