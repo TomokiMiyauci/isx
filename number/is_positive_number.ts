323e7f9312169e1b8bd84051cdc44e38c06833e3
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-13 23:00:02 +0900

    docs(number): update example

--- number/is_positive_number.ts
+++ number/is_positive_number.ts
@@ -8,6 +8,7 @@
  * import { isPositiveNumber } from "https://deno.land/x/isx@$VERSION/number/is_positive_number.ts"
  * import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
  * assertEquals(isPositiveNumber(1), true)
+ * assertEquals(isPositiveNumber(Infinity), true);
  * assertEquals(isPositiveNumber(0), false)
  * ```
  */
